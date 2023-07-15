c9bcf48f6daed2ac38eef06ba970a391ee97c084
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:48:41 +0530

    Add files via upload

---
+++ helloworldwithnewline.java
@@ -0,0 +1,14 @@
+package Jayasrijavapractice;
+
+import java.util.Scanner;
+
+public class helloworldwithnewline {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World\nHello World");
+
+	}
+
+}
